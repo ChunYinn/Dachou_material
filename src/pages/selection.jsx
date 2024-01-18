83acfe42c0efe6bf8ceec6da1f2232e0954ea43c
Chun-Yin Li <chli5570@uni.sydney.edu.au>
2024-01-19 03:01:57 +1100

    update

--- src/pages/selection.jsx
+++ src/pages/selection.jsx
@@ -11,8 +11,16 @@ export default function Selection() {
     navigate('/daily-collect')
   };
 
+  const navigateMaterialSearch = () => {
+    navigate('/material-search');
+  };
+
+  const navigateInventorySearch = () => {
+    navigate('/inventory-search');
+  };
+
   return (
-    <div className="flex justify-center items-center h-screen" style={{ height: 'calc(100vh - 120px)' }}> 
+    <div className="flex flex-col first-letter:justify-center items-center h-screen" style={{ height: 'calc(100vh - 120px)' }}> 
 
       <div className="flex justify-center items-center space-x-20">
         <button
@@ -29,6 +37,24 @@ export default function Selection() {
           onClick={navigateList}
         >
           領料單查詢
+        </button>
+      </div>
+
+      <div className="flex justify-center items-center space-x-20">
+        <button
+          type="button"
+          className="rounded-md bg-gray-600 px-3.5 py-2.5 text-2xl font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600 w-64 h-32"
+          onClick={navigateMaterialSearch}
+        >
+          Material Search
+        </button>
+
+        <button
+          type="button"
+          className="rounded-md bg-gray-600 px-3.5 py-2.5 text-2xl font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600 w-64 h-32"
+          onClick={navigateInventorySearch}
+        >
+          Inventory Search
         </button>
       </div>
     </div>
