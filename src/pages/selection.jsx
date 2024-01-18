6ed5945aab9c919109883de0637a076f0b7a1e8b
Chun-Yin Li <chli5570@uni.sydney.edu.au>
2024-01-19 03:02:23 +1100

    add dialog for export

--- src/pages/selection.jsx
+++ src/pages/selection.jsx
@@ -3,12 +3,13 @@ import { useNavigate } from 'react-router-dom';
 export default function Selection() {
   const navigate = useNavigate();
 
+  // Navigation functions
   const navigateAssignMaterial = () => {
     navigate('/assign');
   };
 
   const navigateList = () => {
-    navigate('/daily-collect')
+    navigate('/daily-collect');
   };
 
   const navigateMaterialSearch = () => {
@@ -20,12 +21,12 @@ export default function Selection() {
   };
 
   return (
-    <div className="flex flex-col first-letter:justify-center items-center h-screen" style={{ height: 'calc(100vh - 120px)' }}> 
-
-      <div className="flex justify-center items-center space-x-20">
+    <div className="flex flex-col justify-center items-center h-screen" style={{ height: 'calc(100vh - 120px)' }}> 
+      <div className="flex justify-center items-center space-x-20 mb-6">
+        {/* First row of buttons */}
         <button
           type="button"
-          className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-2xl font-s font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 w-64 h-32"
+          className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-2xl font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 w-64 h-32"
           onClick={navigateAssignMaterial}
         >
           領料單輸入
@@ -41,6 +42,7 @@ export default function Selection() {
       </div>
 
       <div className="flex justify-center items-center space-x-20">
+        {/* Second row of buttons */}
         <button
           type="button"
           className="rounded-md bg-gray-600 px-3.5 py-2.5 text-2xl font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600 w-64 h-32"
